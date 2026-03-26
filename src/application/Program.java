a7a316cca824ae4577f3c30d679fb34814e78570
Hashimoto1a <newtondainercia@gmail.com>
2026-03-26 15:05:37 -0300

    DepartmentDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,11 +17,11 @@ public class Program{
 		SellerDao sellerdao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== test 1: seller findById ===");
-		Seller seller = sellerdao.findById(3);
+		Seller seller = sellerdao.findById(5);
 		System.out.println(seller);
 		
 		System.out.println("\n=== test 2: seller findByDepartment ===");
-		Department dep = new Department(2, null);
+		Department dep = new Department(1, null);
 		List<Seller> list = sellerdao.findByDepartment(dep);
 		for(Seller obj: list) {
 			System.out.println(obj);
@@ -33,7 +33,7 @@ public class Program{
 			System.out.println(obj);
 		}
 		
-		/*System.out.println("\n=== test 4: seller insert ===");
+		System.out.println("\n=== test 4: seller insert ===");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", LocalDate.of(2021, 01, 01), 4000.0, dep);
 		sellerdao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
@@ -43,7 +43,7 @@ public class Program{
 		seller.setName("Martha");
 		sellerdao.update(seller);
 		System.out.println("update completed");
-		*/
+		
 		System.out.println("\n=== test 6: seller Delete ===");
 		int id = sc.nextInt();
 		sellerdao.deleteById(id);
--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -2,6 +2,7 @@ package model.dao;
 
 import java.util.List;
 
+import model.entities.Department;
 import model.entities.Seller;
 
 public interface DepartmentDao {
@@ -12,8 +13,8 @@ public interface DepartmentDao {
 	
 	void deleteById(Integer id);
 	
-	Seller findById(Integer id);
+	Department findById(Integer id);
 	
-	List<Seller> findAll();
+	List<Department> findAll();
 	
 }
