f55d4ecac7d7dc26fcbb9ec457b56a5ee40730c7
Hashimoto1a <newtondainercia@gmail.com>
2026-03-30 17:08:23 -0300

    delete implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -9,6 +10,7 @@ import model.entities.Department;
 
 public class Program2{
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("=== test 1: Department findById ===");
 		DepartmentDao departmentdao = DaoFactory.createDepartmentDao(); 
@@ -22,12 +24,14 @@ public class Program2{
 		for(Department d: deps) {
 			System.out.println(d);
 		}
+		
 		/*
 		System.out.println("\n=== test 3: Department insert ===");
-		Department dep2 = new Department(null, "Pesquisa");
+		Department dep2 = new Department(null, "D1");
 		DepartmentDao departmentdao2 = DaoFactory.createDepartmentDao();
 		departmentdao2.insert(dep2);
-		*/
+		System.out.println("department inserted");
+		
 		
 		System.out.println("\n=== test 4: Department update ===");
 		Department dep3 = new Department();
@@ -37,5 +41,15 @@ public class Program2{
 		departmentdao.update(dep3);
 		System.out.println("update completed");
 		
+		
+		*/
+		System.out.println("\n=== test 5: Department delete ===");
+		DepartmentDao departmentdao4 = DaoFactory.createDepartmentDao();
+		System.out.println("enter the department's name: ");
+		int id = sc.nextInt();
+		departmentdao4.deleteById(id);
+		System.out.println("department deleted");
+		
+		sc.close();
 	}	
 }
